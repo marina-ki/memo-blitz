61da5a69ee6c6947e0fbcff1893cb8cd39f19d69
k-810 <k810marina@gmail.com>
2020-12-11 04:33:30 +0900

    header, sidebarの大まかな構成

--- app/layouts/Layout.tsx
+++ app/layouts/Layout.tsx
@@ -8,14 +8,15 @@ type LayoutProps = {
 
 const Layout = ({ title, children }: LayoutProps) => {
   return (
-    <>
-      <Head>
-        <title>{title || "memoApp"}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {children}
-    </>
+    <div className="flex bg-gray-100">
+      <aside className="bg-white w-64 min-h-screen flex flex-column">サイドバー</aside>
+      <main className="flex-grow flex flex-col min-h-screen">
+        <header className="bg-indigo-200 border-b h-10 hlex items-center">
+          <h1>ヘッダー</h1>
+        </header>
+        {children}
+      </main>
+    </div>
   )
 }
 
