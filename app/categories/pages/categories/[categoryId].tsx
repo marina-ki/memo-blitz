f8db931224673d9fb605d4de3c07fbdceed56c8d
k-810 <k810marina@gmail.com>
2020-12-11 07:57:44 +0900

    アイコン

--- app/categories/pages/categories/[categoryId].tsx
+++ app/categories/pages/categories/[categoryId].tsx
@@ -4,6 +4,8 @@ import { Link, useRouter, useQuery, useParam, BlitzPage, useMutation } from "bli
 import getCategory from "app/categories/queries/getCategory"
 import deleteCategory from "app/categories/mutations/deleteCategory"
 import { MemosList } from "app/memos/components/MemosList"
+import { PencilOutlineIcon } from "app/components/icons/PencilOutlineIcon"
+import { TrashOutlineIcon } from "app/components/icons/TrashOutlineIcon"
 
 export const Category = () => {
   const router = useRouter()
@@ -13,23 +15,25 @@ export const Category = () => {
 
   return (
     <div>
-      <h1>{category.name}</h1>
-
-      <Link href={`/categories/${category.id}/edit`}>
-        <a>Edit</a>
-      </Link>
-
-      <button
-        type="button"
-        onClick={async () => {
-          if (window.confirm("This will be deleted")) {
-            await deleteCategoryMutation({ where: { id: category.id } })
-            router.push("/categories")
-          }
-        }}
-      >
-        Delete
-      </button>
+      <div className="flex flex-row  w-full m-12 mb-0">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mr-4 text-gray-900">
+          {category.name}
+        </h1>
+        <button>
+          <PencilOutlineIcon className="mr-2" />
+        </button>
+        <button
+          type="button"
+          onClick={async () => {
+            if (window.confirm("This will be deleted")) {
+              await deleteCategoryMutation({ where: { id: category.id } })
+              router.push("/categories")
+            }
+          }}
+        >
+          <TrashOutlineIcon />
+        </button>
+      </div>
 
       <MemosList categoryId={categoryId} />
     </div>
---
+++ app/components/icons/PencilOutlineIcon.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from "react"
+
+type Props = {
+  className?: string
+}
+
+export const PencilOutlineIcon: FC<Props> = ({ className }) => {
+  return (
+    <svg
+      className={`h-6 w-6 text-gray-500 ${className}`}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+      />
+    </svg>
+  )
+}
