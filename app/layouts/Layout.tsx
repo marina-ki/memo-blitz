248ce3feb438a2a4d6d2ba19ae689131f39d316c
k-810 <k810marina@gmail.com>
2020-12-12 13:25:20 +0900

    Revert "Merge pull request #3 from marina-ki/feature/sidebar"
    This reverts commit a13b696675c9e8a555d156a5aab79737d4e2836b, reversing
    changes made to f872e9eb517d6f0172b5b0fa24fe21fb68571dfc.

--- app/layouts/Layout.tsx
+++ app/layouts/Layout.tsx
@@ -1,5 +1,5 @@
-import { ReactNode, Suspense } from "react"
-import Sidebar from "./Sidebar"
+import { ReactNode } from "react"
+import { Head } from "blitz"
 
 type LayoutProps = {
   title?: string
@@ -8,17 +8,14 @@ type LayoutProps = {
 
 const Layout = ({ title, children }: LayoutProps) => {
   return (
-    <div className="flex bg-gray-100">
-      <Suspense fallback="Loading...">
-        <Sidebar />
-      </Suspense>
-      <main className="flex-grow flex flex-col min-h-screen">
-        <header className="bg-indigo-200 border-b h-10 hlex items-center">
-          <h1>ヘッダー</h1>
-        </header>
-        {children}
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>{title || "memoApp"}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {children}
+    </>
   )
 }
 
